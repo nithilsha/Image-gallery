ed8d1f56834a7b951996da5ec9acc13d50f915e5
nithilsha <nithilshanadar@gmail.com>
2024-02-10 01:15:10 +0530

    image-gallery

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Card } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form';
-
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from 'react-image-gallery';
 
 
 
@@ -16,15 +17,30 @@ function Home() {
 useEffect(()=>{
   // here call api
   fetch(
-    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=92b2fae1270fe3d6555ed0f40a6b222a&tags=cat&per_page=10&format=json&nojsoncallback=1"
+ `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=92b2fae1270fe3d6555ed0f40a6b222a&tags=${searchTerm}&per_page=10&format=json&nojsoncallback=1`
   )
-  .then()
-  .then()
+  .then((response) => response.json())
+  .then((data)=>{
+    console.log(data);
+
+    const photos = data.photos.photo;
+const fetchedImages = photos.map((photo)=>({
+
+original :`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_b.jpg`,// this is the main bg photo//
+
+thumbnail:`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_t.jpg`, // this is for small pics showing below the main one// 
+
+description: photo.title
+}));
+
+setImages(fetchedImages);
+
+  })
   .catch( (err)=>{
     console.log(err);
   })
 
-})
+}, [searchTerm]);
 
 const handleInputChange = (event)=>{
 setInputValue(event.target.value);
@@ -35,7 +51,7 @@ setInputValue(event.target.value);
 const handleKeyPress=(event)=>{
 if(event.key=== "Enter"){
   //api call// 
-
+setSearchTerm(inputValue);
 }
 }
 
@@ -57,12 +73,15 @@ if(event.key=== "Enter"){
 
 
 <Card.Body>
-<Form.Control type='text' placeholder='Search ...............' value = {inputValue}
+<Form.Control className = "text-center" type='text' placeholder='Search for some images' value = {inputValue}
 onChange= {handleInputChange}
 onKeyPress = {handleKeyPress}/>
-<h6 className='text-center text-secondary'>
-  Pics of Cat
+<h6 className='text-center text-secondary mb-2 mt-2'>Pics of {searchTerm}
 </h6>
+<ImageGallery items={images} />;
+
+
+
 {/* control is used to create input box */}
 </Card.Body>
 
